cdab36f1efea553cbb326588b4553067a3670b95
Zack <zster05@gmail.com>
2025-02-14 21:06:05 -0500

    lab1 completed

---
+++ cost.py
@@ -0,0 +1,13 @@
+# Part A
+weeks = 16
+classes = 5
+tuition = 6000
+cost_per_week = (tuition / classes) / weeks
+print("Cost per week:", cost_per_week)
+classes_per_week=3
+print(classes_per_week, type(classes_per_week))
+cost_per_week = (6000 / 3) / 16
+print(cost_per_week, type(cost_per_week))
+cost_per_class = 125/3
+print(cost_per_class, type(cost_per_class))
+print(f"cost_per_class is: ${cost_per_class:.2f}. I love this class.")
---
+++ list.py
@@ -0,0 +1,6 @@
+import random
+random_number = random.randint(1, 10)
+print(random_number, type(random_number))
+list_of_sports = ["basketball", "baseball", "football", "soccer", "hockey"]
+random_item = random.choice(list_of_sports)
+print(random_item, type(random_item))
